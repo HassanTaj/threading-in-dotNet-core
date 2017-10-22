06d86e56c6c869e42af79a17c2a6133415d419ce
Lee Haisen <leehaisen01@gmail.com>
2017-10-22 15:52:03 +0500

    Init

---
+++ MultiThreading Demo/MultiThreadingDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadingDemo {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Threads Execution Starts");
+
+            new Thread(() => {
+                for (int i = 0; i < 10; i++) {
+                    Console.WriteLine($"Thread One irritation {i}");
+                    Thread.Sleep(500);
+                }
+            }).Start();
+
+            new Thread(() => {
+                for (int i = 0; i < 10; i++) {
+                    Console.WriteLine($"Thread two irritation {i}");
+                    Thread.Sleep(500);
+                }
+            }).Start();
+               
+        }
+    }
+}
